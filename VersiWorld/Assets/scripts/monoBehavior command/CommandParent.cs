ee5be65b2cf7b195ee27d278d7d7fca5a094391f
Quinn Mosher <quinn.jmosher@gmail.com>
2016-01-08 17:00:47 -0800

    Command Pattern Via Monobehaviors

---
+++ VersiWorld/Assets/scripts/monoBehavior command/CommandParent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandParent : MonoBehaviour {
+
+    protected Color linkColor = Color.gray;
+    protected bool inputLinked = false;
+
+    //have this return some form of function pointer to Activate so Input can only call activate if it has linked to it
+    public virtual void LinkInput(Color in_inputColor)
+    {
+        if (!inputLinked)
+        {
+            inputLinked = true;
+            linkColor = in_inputColor;
+        }
+    }
+
+    //this is overridden to do whatever the action is
+    public virtual void Activate() { /* do nothing */ }
+
+}
---
+++ VersiWorld/Assets/scripts/monoBehavior command/TestInputHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class TestInputHandler : MonoBehaviour {
+
+    public GameObject blueButtonItem;
+    CommandParent blueButtonCommand;
+    KeyCode blueButton = KeyCode.Q;
+
+    // Use this for initialization
+    void Start () {
+        blueButtonCommand = blueButtonItem.GetComponent<CommandParent>();
+        blueButtonCommand.LinkInput(Color.blue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(blueButton))
+        {
+            blueButtonCommand.Activate();
+        }
+	}
+}
