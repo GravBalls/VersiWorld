bac0781feebb27f9ecde2ea891a64243db532f1f
Jeffrey Johnson <jefjohms@gmail.com>
2016-01-13 14:27:53 -0800

    added a magnet object in a separate scene for proof of concept..

---
+++ VersiWorld/Assets/scripts/Magnet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Magnet : MonoBehaviour
+{
+    public float force;
+
+    //BoxCollider magnetTrigger = new BoxCollider();
+
+    void Start()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 position = transform.position;
+        position.y = 0;
+        //transform.position = position;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(position, transform.lossyScale);
+    }
+
+    void OnTriggerStay(Collider other)
+    {   
+        if(other.tag == "Player")
+        {
+            Vector3 direction = Vector3.Normalize(transform.position - other.transform.position);
+            other.GetComponent<Rigidbody>().AddForce(direction * force * Time.fixedDeltaTime);
+            Debug.Log(direction);
+        }
+    }
+
+}
