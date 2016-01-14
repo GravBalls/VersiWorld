224b6fa8d4451efdd67c83d74b9e72444c99b7c9
Quinn Mosher <quinn.jmosher@gmail.com>
2016-01-14 08:53:24 -0800

    material swapping script start

---
+++ VersiWorld/Assets/scripts/PlatformScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformScript : CommandParent {
+
+    public bool isActivatable = false;
+    bool isFirstState = true;
+
+    public string startingMaterialName;
+    public PhysicMaterial startingMaterial;
+    public string endingMaterialName;
+    public PhysicMaterial endingMaterial;
+
+    BoxCollider myBoxCollider;
+
+    // Use this for initialization
+    void Start () {
+        myBoxCollider = GetComponent<BoxCollider>();
+
+        myBoxCollider.material = startingMaterial;
+        
+        //default link color
+        actionDescription = startingMaterialName;
+	}
+
+    public override void Activate()
+    {
+        if (isActivatable && inputLinked)
+        {
+            if (isFirstState)
+            {
+                myBoxCollider.material = endingMaterial;
+                actionDescription = endingMaterialName;
+            }
+            else
+            {
+                myBoxCollider.material = startingMaterial;
+                actionDescription = startingMaterialName;
+            }
+            isFirstState = !isFirstState;
+        }
+    }
+}
