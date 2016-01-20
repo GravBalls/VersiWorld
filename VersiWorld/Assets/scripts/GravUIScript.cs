e82eb13bb9f5ec84862a5385a6a24ede7bbb3979
Quinn Mosher <quinn.jmosher@gmail.com>
2016-01-20 15:55:07 -0800

    Gravity UI stuf

---
+++ VersiWorld/Assets/scripts/GravUIScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GravUIScript : MonoBehaviour {
+
+    Image gravImage;
+    GravityCommand gravControler = null;
+
+    public Sprite iconUp;
+    public Sprite iconRight;
+    public Sprite iconDown;
+    public Sprite iconLeft;
+
+    // Use this for initialization
+    void Start () {
+        gravImage = GetComponentInChildren<Image>();
+        gravControler = FindObjectOfType<GravityCommand>();
+        if (gravControler == null) {
+            gravImage.sprite = iconDown;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gravControler != null) {
+            switch (gravControler.currentDirection)
+            {
+                case GravityCommand.DIRECTION.UP:
+                    gravImage.sprite = iconUp;
+                    break;
+                case GravityCommand.DIRECTION.RIGHT:
+                    gravImage.sprite = iconRight;
+                    break;
+                case GravityCommand.DIRECTION.DOWN:
+                    gravImage.sprite = iconDown;
+                    break;
+                case GravityCommand.DIRECTION.LEFT:
+                    gravImage.sprite = iconLeft;
+                    break;
+            }
+        }
+	}
+}
