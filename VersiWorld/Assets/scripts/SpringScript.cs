6b6bb3873f94f59bcab542dd5d359b9f35046a1d
Quinn Mosher <quinn.jmosher@gmail.com>
2016-01-20 17:41:46 -0800

    start on springs

---
+++ VersiWorld/Assets/scripts/SpringScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringScript : CommandParent {
+
+    public Vector3 bounceDirection = new Vector3(0, 1, 0);
+    public bool isActivateable = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
