9d6c289a6ec83bcb1f711d67df6e350a12eff1ae
Quinn Mosher <quinn.jmosher@gmail.com>
2016-01-21 11:24:37 -0800

    spring script

--- VersiWorld/Assets/scripts/SpringScript.cs
+++ VersiWorld/Assets/scripts/SpringScript.cs
@@ -4,15 +4,67 @@ using System.Collections;
 public class SpringScript : CommandParent {
 
     public Vector3 bounceDirection = new Vector3(0, 1, 0);
-    public bool isActivateable = false;
+    public float bounceForce = 10;
+    public bool isActivatable = false;
+
+    int maxActiveFrames = 10;
+    int curActiveFrames = 0;
+    bool isActive = false;
+
+    Rigidbody colidedObject = null;
 
 	// Use this for initialization
 	void Start () {
-	
+        bounceDirection.Normalize();
+
+        if (isActivatable) {
+            actionDescription = "Activate Spring";
+        }
+        else
+        {
+            isActive = true;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (isActivatable) {
+            if (isActive)
+            {
+                curActiveFrames++;
+                if (curActiveFrames > maxActiveFrames)
+                {
+                    isActive = false;
+                }
+            }
+        }
+    }
+
+    public override void Activate()
+    {
+        if (isActivatable)
+        {
+            isActive = true;
+            curActiveFrames = 0;
+            if (colidedObject != null)
+            {
+                colidedObject.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
+                colidedObject = null;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isActive)
+        {
+            collision.rigidbody.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
+        }
+        else
+        {
+            colidedObject = collision.rigidbody;
+        }
+    }
+
 }
