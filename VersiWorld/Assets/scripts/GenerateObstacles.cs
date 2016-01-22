c6ed1dfba49c0d8fb4ab16a7b62791d7a3f8efed
Jeffrey Johnson <jefjohms@gmail.com>
2016-01-22 13:26:49 -0800

    have a mechanic that's kind of interesting, a little different then plan, but I'm fleshing out.

---
+++ VersiWorld/Assets/scripts/GenerateObstacles.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GenerateObstacles : MonoBehaviour
+{
+    public int quantity;
+    public Transform minPosition;
+    public Transform maxPosition;
+    public GameObject prefab;
+    public float placementBuffer = .01f;
+    
+
+    List<GameObject> obstacles = new List<GameObject>();
+    float prefabHalfWidth;
+    void Awake()
+    {
+        prefabHalfWidth = prefab.GetComponent<Collider>().bounds.size.x * 0.5f;
+        for (int i = 0; i < quantity; i++)
+        {
+            GameObject obstacle = Instantiate(prefab, GetRandomSpawnPosition(), Quaternion.identity) as GameObject;
+            obstacles.Add(obstacle);
+        }
+
+    }
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        Vector3 result = new Vector3();
+        int fuse = 0;//safety fuse for killing infinite loop
+        while (true)
+        {
+            result = minPosition.position;
+            result.x = Random.Range(minPosition.position.x, maxPosition.position.x);
+           if(CheckIfClear(result))
+            {
+                return result;
+            }
+            Debug.Assert(++fuse < 100, "Safety fuse blown on infinite loop.");
+            
+
+        }
+    }
+
+    bool CheckIfClear(Vector3 position)
+    {
+        bool result = true;
+        foreach (GameObject obstacle in obstacles)
+        {
+            float minX = obstacle.transform.position.x - (prefabHalfWidth + placementBuffer);
+            float maxX = obstacle.transform.position.x + (prefabHalfWidth + placementBuffer);
+            if (position.x > minX && position.x < maxX)//obstacle already there no need to search anymore get another random and try again
+            {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ VersiWorld/Assets/scripts/Obstacle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour
+{
+    ParticleSystem particles;
+    Renderer renderer;
+
+    void Awake()
+    {
+        particles = GetComponent<ParticleSystem>();
+        renderer = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            particles.Play();
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            StartCoroutine(Die());
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Die());
+        }
+
+    }
+
+    IEnumerator Die()
+    {
+        renderer.enabled = false;
+        particles.Play();
+        while(particles.isPlaying)
+        {
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
